7630e0b5e64eade6eda199584ea2e776a2bd9714
Bulemero <bulemero@gmail.com>
2019-03-26 15:54:17 +0100

    first commit

---
+++ MyNindieStore.js
@@ -0,0 +1,87 @@
+var myVueObject = new Vue({
+    el: "#app",
+    data: {
+
+        url: "remember you need to upload your JSON",
+        games: [
+            {
+                "title": "Cadence of Hyrule",
+                "subtitle": "In the latest rhythmic action-adventure from Brace Yourself Games, you can enjoy the gameplay of Crypt of the Necrodancer in the setting of The Legend of Zelda series.",
+
+                "main_image": "https://cdn02.nintendo-europe.com/media/images/10_share_images/games_15/nintendo_switch_download_software_1/H2x1_NSwitchDS_CadenceOfHyruleCryptOfTheNecroDancerFeaturingTheLegendOfZelda_image1600w.png",
+                "trailer": "https://www.youtube.com/watch?v=oYd_pph6RnI",
+                "thumbnail": "https://media.nintendo.com/nintendo/bin/CMNZXHGJws8aXRyQ14jlYRG0HQTWaWbF/eM-J-xwx1dNv9eBahzh16CF0-J5BohWz.png",
+
+                "carousel": ["https://media.nintendo.com/nintendo/bin/8KsTCulX3sif9q2ATfAq_RkjfKa-oDf-/DfixcCTdDmo-79kXGUTTFTWYqeUVNcFH.jpg", "https://media.nintendo.com/nintendo/bin/rrhtHBlujxsi3patWOHmn4P47c29O0CP/KFLXmQKsy0UtkLgGraHgbp84Ycu_dc9f.jpg", "https://media.nintendo.com/nintendo/bin/83gHWVdLDNmpyR4xALwdv-PLhScex8Pc/lNgkNL9rqmhZbT7emdzwouu0TctRC36u.jpg", "https://media.nintendo.com/nintendo/bin/xioDCFX3jI-b1hcZQT2-0E9uPyLLtfZy/xQCUT6qnKktIUu4-Tj-tUk3obL4HW7SH.jpg", "https://media.nintendo.com/nintendo/bin/g2FYhXsmbxDlHQ6zM1uF2DJ0PlSz57cJ/ZILbHx4L4WmCOBTFNcNXrD9GbJejs5M-.jpg", "https://o.aolcdn.com/images/dims?quality=85&image_uri=https%3A%2F%2Fo.aolcdn.com%2Fimages%2Fdims%3Fcrop%3D1429%252C755%252C0%252C0%26quality%3D85%26format%3Djpg%26resize%3D1600%252C845%26image_uri%3Dhttps%253A%252F%252Fs.yimg.com%252Fos%252Fcreatr-uploaded-images%252F2019-03%252Fbaafd630-4b30-11e9-bffa-c1d170acc868%26client%3Da1acac3e1b3290917d92%26signature%3Dcdeafa4fad05da3eeb0c92552510c5ed6ca35064&client=amp-blogside-v2&signature=a526d3c196f3ff3a294630607f7ffd72b08006e0"],
+
+                "game_info": "As Link—or even as Princess Zelda—you’ll explore the randomly generated overworld and dungeons on a quest to save Hyrule. Every beat of each remixed, The Legend of Zelda tune is a chance to move, attack, defend, and more, so stay one step ahead of each enemy and boss…or face the music. From modern-looking Lynels to the Hyrulean Soldiers of old, you must master the instinctive movements of each pixel-art enemy, and strategically outstep them in rhythmic combat. So equip yourself with an arsenal of iconic Legend of Zelda items, as well as the spells and weapons from Crypt of the Necrodancer. You must be prepared to face Hyrule’s most wicked bosses, which have been transformed by magic, granting them mighty, new forms! A rhythmic action-adventure game in the world of the Legend of Zelda™ series. Explore the randomly generated overworld and dungeons to save Hyrule as Link or Princess Zelda. Use items from the Legend of Zelda™ series, plus spells and weapons from the Crypt of the Necrodancer game. Outstep legendary enemies and bosses in rhythmic combat. Keep the beat with 25 remixed classic Legend of Zelda™ tunes.",
+
+                "category": "Action, Adventure, Music",
+                "price": "Pending",
+                "release_date": "Spring 2019",
+                "number_of_players": "1-2",
+                "pegi": "Pending",
+                "developer": "Brace Yourself Games",
+                "publisher": "Nintendo",
+               },
+
+            {
+                "title": "Cuphead",
+                "subtitle": "The all-cartoon Magical Wondergame!",
+
+                "main_image": "https://cdn02.nintendo-europe.com/media/images/10_share_images/games_15/nintendo_switch_download_software_1/H2x1_NSwitchDS_Cuphead_image1600w.jpg",
+                "trailer": "https://www.youtube.com/watch?v=GjZupy36_sM",
+                "thumbnail": "https://cdn02.nintendo-europe.com/media/images/11_square_images/games_18/nintendo_switch_download_software/SQ_NSwitchDS_Cuphead.jpg",
+
+                "carousel": ["https://cdn02.nintendo-europe.com/media/images/06_screenshots/games_5/nintendo_switch_download_software_2/nswitchds_cuphead/NSwitchDS_Cuphead_01.jpg", "https://cdn02.nintendo-europe.com/media/images/06_screenshots/games_5/nintendo_switch_download_software_2/nswitchds_cuphead/NSwitchDS_Cuphead_02.jpg", "https://cdn02.nintendo-europe.com/media/images/06_screenshots/games_5/nintendo_switch_download_software_2/nswitchds_cuphead/NSwitchDS_Cuphead_03.jpg", "https://cdn02.nintendo-europe.com/media/images/06_screenshots/games_5/nintendo_switch_download_software_2/nswitchds_cuphead/NSwitchDS_Cuphead_04.jpg", "https://cdn02.nintendo-europe.com/media/images/06_screenshots/games_5/nintendo_switch_download_software_2/nswitchds_cuphead/NSwitchDS_Cuphead_05.jpg", "https://cdn02.nintendo-europe.com/media/images/06_screenshots/games_5/nintendo_switch_download_software_2/nswitchds_cuphead/NSwitchDS_Cuphead_06.jpg"],
+
+                "game_info": "Cuphead is a classic run and gun action game heavily focused on boss battles. Inspired by cartoons of the 1930s, the visuals and audio are painstakingly created with the same techniques of the era: traditional hand drawn cel animation, watercolor backgrounds, and original jazz recordings. Play as Cuphead or Mugman (in single player or local co-op) as you traverse strange worlds, acquire new weapons, learn powerful super moves, and discover hidden secrets while you try to pay your debt back to the devil!",
+
+                "category": "Action, Arcade, Platformer",
+                "price": "19.99€",
+                "release_date": "Apr 18, 2019",
+                "number_of_players": "1-2",
+                "pegi": "7",
+                "developer": "Studio MDHR",
+                "publisher": "Studio MDHR",
+               },
+
+
+
+                    ],
+        
+        search: ""
+    },
+
+    //    created() {
+    //        this.getData()
+    //    },
+    //
+    //    methods: {
+    //        //------------------------------Fetch data -------------------------------------------------------------->
+    //        getData() {
+    //
+    //            fetch('https://api.myjson.com/bins/udbm5')
+    //                .then(response => {
+    //                    return response.json();
+    //                }).then(data => {
+    //                    // Work with JSON data here
+    //                    console.log(data);
+    //                    this.books = data.books;
+    //                    //                this.myVueObject.books = data.books;
+    //
+    //
+    //                }).catch(err => {
+    //                    // Do something for an error here
+    //                    console.log(error);
+    //                });
+    //        }
+    //    },
+    //
+    //    computed: {
+    //        filteredBooks: function () {
+    //
+    //            return this.books.filter((item) => item.titulo.toLowerCase().match(this.search.toLowerCase()));
+    //        }
+    //    },
+});
---
+++ readme.txt
@@ -0,0 +1,26 @@
+Exercise for Front-End Profiles:
+
+During the next weeks you will be working with a specific project. This project has all the functionality that we already seen and they are typical in all websites/platforms.
+
+Your task
+- Develop/cereate an e-commerce, with shopping cart and the same functionalities as in the bookstore and cart exercice.
+- Each product has to have minimum this properties:
+    Title, subtitle, images, description, category, price.
+- Timming: 2 weeks to deliver using pure JS (Aprox.)
+- Discount: every 4 products you buy you have to apply a discount. (pay 3 receive 4). If you buy more than 5 items in total a 10% discount is apllied to the total price.
+
+Minimum requirements:
+
+Create a json with the necessary data for each book. You can use real APIs like Google Books or Amazon. Or you can create your own JSON with your own tematic (The structure has to respect the minimum properties.)
+You can modify the original JSON of the Bookstore exercise but modifying it with more properties, besides the one described previously you can use price, theme, stock and others ...
+The json must be hosted in a real URL and must be accessed via fetch.
+The user must be able to visualize all the products, filter them by properties (theme, title, price …) or several combinations.
+Each product must be added to the shopping cart and the user can know at all times how many products are in the cart and the price.
+In the same way you can remove the products from the car.
+The discount condition in the total price will be the same as described before. The 4th free product and discount for the number of items.
+The design must be professional and respoonsive to all screens (Specific CSS for mobile, tablet and desktop)
+No mistakes are accepted in the long term. Remember your platform has to keep working even if we add 1000 products.
+No login needed (For now...)
+
+
+https://elviraleem.github.io/Shoppin-Cart/
